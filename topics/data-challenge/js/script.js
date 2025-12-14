6da5c039b9ccc05b65bfaf4b07ba47b81fe576fb
abhivoleti <voletiabhi@gmail.com>
2025-12-14 16:20:18 -0500

    data challenge completed

---
+++ topics/data-challenge/js/script.js
@@ -0,0 +1,67 @@
+/**
+ * Tsnrams
+ * Abhinav Voleti
+ * 
+ * A program to generate new car model names using dinosaurs.
+ * 
+ * Uses:
+ * Darius Kazemi's corpora repository
+ * https://github.com/dariusk/corpora/tree/master
+ */
+
+"use strict";
+
+let carData = undefined;
+let dinosaurData = undefined;
+let langData = undefined;
+let lang = "fr";
+
+// Starts with the instruction
+let carName = "Click to generate a car name.";
+let dinosaurName = "click to generate a dinosaur name.";
+
+/**
+ * Load the car and dinosaur data
+ */
+function preload() {
+    carData = loadJSON("assets/data/cars.json");
+    dinosaurData = loadJSON("assets/data/dinosaurs.json");
+}
+
+/**
+ * Create the canvas
+*/
+function setup() {
+    createCanvas(600, 400);
+}
+
+/**
+ * Display the current main text (either instructions or a car)
+*/
+function draw() {
+
+    background(0);
+
+    push();
+    fill("pink");
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    text(carName, width / 2, height / 2);
+    pop();
+
+    push();
+    fill("yellow");
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    text(dinosaurName, width / 2, height / 4);
+    pop();
+}
+
+/**
+ * Generate a new car name
+ */
+function mousePressed() {
+    carName = random(carData.cars);
+    dinosaurName = random(dinosaurData.dinosaurs);
+
+}
